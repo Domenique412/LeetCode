b1ac5c39c66463fdff849785d7566a70aff01b4f
Domenique Watkins-Wise <209522385+Domenique412@users.noreply.github.com>
2026-02-02 15:23:50 -0500

    Time: 1 ms (87.59%), Space: 56.8 MB (18.71%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const seen = new Map();
+
+    for (let i=0; i < nums.length; i++){
+ const need = target - nums[i]; 
+ if (seen.has(need)){
+    return [seen.get(need), i ];
+ }
+ seen.set(nums[i], i);
+ }
+};
+
+
+ 
