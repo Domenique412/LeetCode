842245541d0c8d62e17bab3e5ac1ebe8b7a66610
Domenique Watkins-Wise <209522385+Domenique412@users.noreply.github.com>
2026-02-04 15:18:22 -0500

    Time: 10 ms (96.45%), Space: 73.7 MB (68.43%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const seen = new Set();
+
+    for (let i = 0; i < nums.length; i++){
+        const current = nums[i]; 
+
+        if (seen.has(current)){
+            return true;
+        }
+        seen.add(current);
+    }
+    return false;
+};
