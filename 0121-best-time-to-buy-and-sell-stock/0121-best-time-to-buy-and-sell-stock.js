97e8730b894a0485d4eb3c67b63052187ccd4330
Domenique Watkins-Wise <209522385+Domenique412@users.noreply.github.com>
2026-02-07 13:03:35 -0500

    Time: 1 ms (95.24%), Space: 65.7 MB (26.59%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let minPrice = prices[0];
+    let bestProfit = 0;
+
+    for (let i = 0; i < prices.length; i ++){
+         const priceThatDay = prices[i];
+
+         if (priceThatDay < minPrice){
+            minPrice = priceThatDay;
+         }else{
+            const profitSellThatDay = priceThatDay - minPrice;
+
+        if (profitSellThatDay > bestProfit){
+            bestProfit = profitSellThatDay;
+        }
+         }
+    }
+    return bestProfit;
+};
